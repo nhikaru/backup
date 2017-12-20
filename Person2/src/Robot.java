ebb6967f3993e32917a3b25a4bc4b75c8db14b22
nhikaru <hika.l.ys.56@gmail.com>
2017-12-20 16:36:16 +0900

    12/20

---
+++ Person2/src/Robot.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+
+/**
+ * @author internousdev
+ *
+ */
+public class Robot {
+	public String name=null;
+
+	public void talk(){
+		System.out.println(this.name+"が流暢に話す");
+	}
+	public void walk(){
+		System.out.println(this.name+"がゆっくり歩く");
+	}
+	public void run(){
+		System.out.println(this.name+"が速く走る");
+	}
+}
