baf6824f0eb79279c4b69b582a021ebae97e4f56
nhikaru <hika.l.ys.56@gmail.com>
2018-01-31 18:01:27 +0900

    1/31

--- ecsitear/src/com/internousdev/ecsite/action/LoginAction.java
+++ ecsitear/src/com/internousdev/ecsite/action/LoginAction.java
@@ -3,6 +3,10 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.internousdev.ecsite.dao.BuyItemDAO;
+import com.internousdev.ecsite.dao.LoginDAO;
+import com.internousdev.ecsite.dto.BuyItemDTO;
+import com.internousdev.ecsite.dto.LoginDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport implements SessionAware{
@@ -10,7 +14,7 @@ public class LoginAction extends ActionSupport implements SessionAware{
 	private String loginPassword;
 	public Map<String,Object>session;
 	private LoginDAO loginDAO = new LoginDAO();
-	private LoginDTO lodinDTO = new LoginDTO();
+	private LoginDTO loginDTO = new LoginDTO();
 	private BuyItemDAO buyItemDAO = new BuyItemDAO();
 
 	public String execute(){
@@ -23,9 +27,33 @@ public class LoginAction extends ActionSupport implements SessionAware{
 			BuyItemDTO buyItemDTO = buyItemDAO.getBuyItemInfo();
 
 			session.put("login_user_id",loginDTO.getLoginId());
+			session.put("id",buyItemDTO.getId());
+			session.put("buyItem_name",buyItemDTO.getItemName());
+			session.put("buyItem_price",buyItemDTO.getItemPrice());
+
+			return result;
 
 		}
+		return result;
 
+	}
+
+	public String getLoginUserId(){
+		return loginUserId;
+	}
+	public void setLoginUserId(String loginUserId){
+		this.loginUserId=loginUserId;
+	}
+	public String getLoginPassword(){
+		return loginPassword;
+	}
+	public void setLoginPassword(String loginPassword){
+		this.loginPassword=loginPassword;
+	}
+
+	@Override
+	public void setSession(Map<String,Object>session){
+		this.session=session;
 	}
 
 }
